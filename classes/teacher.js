7d561d61585a2e7460e9760210d1912c74659aeb
kheisler4417 <kheisler4417@gmail.com>
2023-03-25 16:12:35 -0400

    my fork final

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,37 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    let totalExperience = 0;
+    for (const teacher of teachers) {
+      totalExperience += teacher.yearsOfExperience;
+    }
+    return totalExperience;
+  }
+}
+
+// class Teacher extends Person {
+//   constructor(firstName, lastName, age, subject, yearsOfExperience) {
+//     super(firstName, lastName, age);
+//     this.subject = subject;
+//     this.yearsOfExperience = yearsOfExperience;
+//   }
+
+//   static combinedYearsOfExperience(teachers) {
+//     let totalExperience = 0;
+//     for (const teacher of teachers) {
+//       totalExperience += teacher.yearsOfExperience;
+//     }
+//     return totalExperience;
+//   }
+// }
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
